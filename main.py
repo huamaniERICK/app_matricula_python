93cc14a12d2421ddad8196acb89e41fd3fbb8e8f
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2024-10-14 08:40:46 -0500

    actualizo mis tareas

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+- registrar alumnos.
+- generar fichas de matricula
+- mostrar la lista de todos los matriculados
+- filtrar matriculados por programa de estudio
+"""
+lista_alumnos=[]
+#inicio de problema
+#necesito poder agregar mas alumnos sin neceisdad de crear tantas variables
+#posible solucion cre o encerar el codigo en un cliclo while
+
+while True:
+    opcion=input
+
+    if opcion.lower()=="n"
+    break
+
+nombre=input("Ingrese el nombre del alumno: ")
+apellido=input("Ingrese el apeliido del alumno: ")
+alumno=(
+    "nombre":nombre,
+    "apellido":pellido
+)
+lista_alumnos.append(alumno)
+
+print(lista_alumnos)
+
+
+
